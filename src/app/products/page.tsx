5cddd7494293e71a4c88229617370acb01f35f7c
kareem <kareem693sd@gmaile.com>
2025-12-26 19:36:20 +0530

    add

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,21 +2,20 @@ import { getAllProducts } from "@/services/productService";
 import SearchBar from "@/components/SearchBar";
 import { Product } from "@/types/product";
 
+export const runtime = "nodejs"; 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
   title: "All Products",
   description: "Browse all available products",
 };
 
-export const dynamic = "force-dynamic";
-
 export default async function ProductsPage() {
   console.log("PRODUCTS PAGE RENDERING ON VERCEL");
 
-  // Fetch directly from FakeStore API (no /api needed)
   const products: Product[] = await getAllProducts();
 
   console.log("PRODUCTS LENGTH:", products.length);
-  console.log("PRODUCTS DATA:", products);
 
   return <SearchBar products={products} />;
 }
